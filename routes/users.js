f082735d42c84b17fd1fa81364959d1daa665465
saba_jmukhadze <sabajmukhadze23@gmail.com>
2023-06-30 20:59:23 +0400

    users updating and deleting logic simplified

--- routes/users.js
+++ routes/users.js
@@ -4,44 +4,41 @@ const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
+  try {
     if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
       req.body.password = await bcrypt.hash(req.body.password, salt);
     }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
-      res.status(200).json(updatedUser);
-    } catch (err) {
-      res.status(500).json(err);
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!updatedUser) {
+      return res.status(404).json("User not found");
     }
-  } else {
-    res.status(401).json("You can update only your account!");
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("An error occurred while updating the user");
   }
 });
 
 router.delete("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
-      try {
-        await Post.deleteMany({ email: user.email });
-        await User.deleteOne({ _id: req.params.id });
-        res.status(200).json("User has been deleted!");
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } catch (err) {
-      res.status(404).json("user not found", err);
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json("User not found");
     }
-  } else {
-    res.status(401).json("You can delete only your account!");
+
+    await Post.deleteMany({ email: user.email });
+    await User.deleteOne({ _id: req.params.id });
+
+    res.status(200).json("User has been deleted!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("An error occurred while deleting the user");
   }
 });
 
